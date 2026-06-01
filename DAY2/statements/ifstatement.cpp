9d119a03a46b643d0b4dba5df36647b67d5819ef
Prachi Charlewar <sakshicharlewar4@gmail.com>
2026-06-01 21:26:14 +0530

    DAY2

---
+++ DAY2/statements/ifstatement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 16;
+    if (age >= 18)
+    {
+        cout << "you are eligible for Driving licence";
+    }else{
+        cout<<"Not eligible";
+    }
+
+    return 0;
+}
